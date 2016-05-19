a3baa83a318ca4030fdd972d3ae35e2858bd7ba7
crisng4 <cristina.ng4@gmail.com>
2016-05-19 23:40:06 +0200

    He realizado el ejercicio

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from models import Pages
+from django.http import HttpResponse, HttpResponseNotFound
+
+ # Create your views here.
+
+def nueva(request,name):
+    try:
+        pagina = Pages.objects.get(name = name)
+        return HttpResponse(pagina.page)
+    except Pages.DoesNotExist:
+        return HttpResponse("<h3> Pagina no encontrada, " + name + "</h3>")
+
+def guardar(request,name,url):
+    pagina = Pages(name = name,page = url)
+    pagina.save()
+    return HttpResponse(" Pagina guardada: "+name)
+
+def mostrar(request):
+    respuesta = ""
+    lista_pags = Pages.objects.all()
+    for pag in lista_pags:
+        respuesta+="<li>"+ pag.name + " : " + pag.page + "</li>"
+    return HttpResponse(respuesta)
+
+def Error404(request,cont1,cont2):
+    return HttpResponse (" Error404 Not Found")
